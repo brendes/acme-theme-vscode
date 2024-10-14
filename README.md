3280e5bfc84fb4555da8f4eace0db9d6481f0c50
Brendan Desmond <brendes@gmail.com>
2024-10-14 19:40:59 -0400

    add white theme

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 
 VS Code theme inspired by the [Acme editor](https://en.wikipedia.org/wiki/Acme_%28text_editor%29). 
 
-One big difference from the original Acme editor is that comments and strings are highlighted in respective shades of gray.
-The other is that the editor colors are slightly muted to accomodate modern displays.
+Two big differences between the this Acme theme and the original Acme editor's color scheme:
+1. Comments and strings are highlighted in respective shades of off-gray.
+2. Editor colors are slightly desaturated to accomodate modern displays.
+
+A blue and white version of the theme is also included.
 
 ## Screenshots
+
 TODO
 
 ## Build
 
 Requires Python ≥ 3.11.
 - Build themes: `make build`
-- Build extension: `make package`
+- Build extension: `make clean package`
--- src/build.py
+++ src/build.py
@@ -13,9 +13,13 @@ colors = {
     "black_2": "#333333",
     "gray_1": "#888888",
     "gray_2": "#cccccc",
-    "gray_3": "#dddddd",
-    "blue_1": "#5577bb",
-    "blue_2": "#99bbdd",
+    "gray_3": "#dfdfdf",
+    "gray_4": "#eeeeee",
+    "gray_5": "#f8f8f8",
+    "blue_1": "#5580bf",
+    "blue_2": "#a0c0dd",
+    "blue_3": "#c8e0ee",
+    "blue_4": "#f0f8ff",
     "cyan_1": "#559999",
     "cyan_2": "#99cccc",
     "cyan_3": "#aaeeee",
@@ -27,6 +31,7 @@ colors = {
     "magenta_1": "#996699",
     "magenta_2": "#bb99bb",
     "orange_1": "#d08770",
+    "orange_2": "#f0ccb0",
     "purple_1": "#8888cc",
     "purple_2": "#bbbbff",
     "red_1": "#bb5d5d",
@@ -39,21 +44,40 @@ colors = {
     "invisible": "#00000000",
 }
 
-acme_theme = {
+acme = {
     **colors,
+    "theme_name": "Acme",
     "fg": colors["black_1"],
     "dim": colors["black_1"] + "88",
     "fade": colors["black_1"] + "66",
     "faint": colors["black_1"] + "44",
-    "ui_bg": colors["cyan_4"],
-    "ui_hl": colors["cyan_3"],
     "selection_bg": colors["yellow_2"],
+    "match_bg": colors["purple_2"],
     "button_bg": colors["purple_1"],
     "button_fg": colors["white"],
+    "ui_bg": colors["cyan_4"],
+    "ui_hl": colors["cyan_3"],
+    "hover_bg": colors["white"],
+    "hover_hl": colors["cyan_3"]
 }
 
+acme_white = {
+    **acme,
+    "theme_name": "Acme White",
+    "bg_1": colors["white"],
+    "bg_2": colors["gray_5"],
+    "bg_3": colors["gray_4"],
+    "selection_bg": colors["gray_3"],
+    "match_bg": colors["orange_2"],
+    "button_bg": colors["purple_1"],
+    "button_fg": colors["white"],
+    "ui_bg": colors["blue_4"],
+    "ui_hl": colors["blue_3"],
+    "hover_bg": colors["gray_5"],
+    "hover_hl": colors["gray_3"]
+}
 
-def generate_theme(template_file, output_file):
+def generate_theme(theme_dict, template_file, output_file):
     """Generate a color-theme JSON file based on a theme dictionary."""
     with open(template_file, "r") as f:
         theme_template = yaml.safe_load(f)
@@ -61,7 +85,7 @@ def generate_theme(template_file, output_file):
     # Format the strings in the YAML dictionary
     def recursive_format(value):
         if isinstance(value, str):
-            return value.format(**acme_theme)
+            return value.format(**theme_dict)
         elif isinstance(value, list):
             return [recursive_format(item) for item in value]
         elif isinstance(value, dict):
@@ -76,9 +100,11 @@ def generate_theme(template_file, output_file):
 
 
 def main():
-    output_file_name = "acme-color-theme.json"
-    output_file_path = os.path.join("themes", output_file_name)
-    generate_theme("src/template.yml", output_file_path)
+    themes = {"acme": acme, "acme_white": acme_white}
+    for theme_name, theme_dict in themes.items():
+        output_file_name = f"{theme_name.replace('_', '-')}-color-theme.json"
+        output_file_path = os.path.join('themes', output_file_name)
+        generate_theme(theme_dict, 'src/template.yml', output_file_path)
 
 
 if __name__ == "__main__":
