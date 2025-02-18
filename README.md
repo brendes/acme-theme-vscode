958b804f58631c25ba342ca0244109becd09b881
Brendan Desmond <brendes@gmail.com>
2025-02-18 12:34:22 -0500

    Simplify

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 VS Code theme inspired by the [Acme editor](https://en.wikipedia.org/wiki/Acme_%28text_editor%29). 
 
 Two big differences between this theme and the original Acme editor's color scheme:
-1. Comments are a faded color.
+1. Comments are a faded foreground color.
 2. Some other editor and/or UI colors are tweaked from the original Plan9/Acme color scheme to look better on modern displays.
 
 A blue and white version of the theme is also included.
@@ -16,5 +16,4 @@ TODO
 
 Requires vsce and Python ≥ 3.11.
 - Build themes: `make build`
-- Build extension: `make clean package`
 - Build extension: `make clean package`
--- src/build.py
+++ src/build.py
@@ -4,10 +4,11 @@ import json
 import os
 
 colors = {
-    "paleyellow_1": "#ffffea",
-    "paleyellow_2": "#f2f2dd",
-    "paleyellow_3": "#eeeed9",
-    "paleyellow_4": "#ddddc8",
+    "paleyellow_1": "#ffffee",
+    "paleyellow_2": "#f2f2e2",
+    "paleyellow_3": "#eeeede",
+    "paleyellow_4": "#ddddcd",
+    "paleyellow_5": "#777770",
     "black": "#000000",
     "gray_1": "#888888",
     "gray_2": "#e2e2e2",
@@ -19,7 +20,7 @@ colors = {
     "orange_1": "#dd9870",
     "orange_2": "#f8c090",
     "yellow_1": "#99884c",
-    "yellow_2": "#eeeea0",
+    "yellow_2": "#eeeeaa",
     "green_1": "#558855",
     "green_2": "#a8cca8",
     "green_3": "#eeffee",
@@ -27,78 +28,53 @@ colors = {
     "cyan_2": "#99cccc",
     "cyan_3": "#aeeeee",
     "cyan_4": "#eeffff",
-    "blue_1": "#4466bb",
+    "blue_1": "#586dbb",
     "blue_2": "#a4c4e0",
     "blue_3": "#cce4f2",
-    "blue_4": "#f0f8ff",
+    "blue_4": "#f2faff",
     "purple_1": "#8888cc",
     "purple_2": "#bbbbff",
     "magenta_1": "#aa6688",
     "magenta_2": "#cc88aa",
-    "border": "#00000088",
+    "border": "#00000078",
     "invisible": "#00000000",
 }
 
 base_theme = {
     **colors,
     "theme_type": "light",
+    "bg_1": colors["paleyellow_1"],
+    "bg_2": colors["paleyellow_2"],
+    "bg_3": colors["paleyellow_3"],
+    "blank_bg": colors["white"],
+    "hover_bg": colors["white"],
+    "selection_bg": colors["yellow_2"],
     "fg": colors["black"],
     "fg_dim": colors["black"] + "6d",
     "fg_faint": colors["black"] + "44",
     "fg_ghost": colors["black"] + "20",
-    "blank_bg": colors["white"],
-    "neutral_hl": colors["black"] + "10"
+    "neutral_hl": colors["black"] + "10",
+    "match_bg": colors["purple_2"] + "ee",
+    "badge_bg": colors["purple_1"],
+    "badge_fg": colors["white"],
+    "button_bg": colors["purple_1"],
+    "button_fg": colors["white"]
 }
 
 themes = {
     "acme": {
         **base_theme,
         "theme_name": "Acme",
-        "bg_1": "#ffffee",
-        "bg_2": "#f2f2e2",
-        "bg_3": "#eeeede",
-        "selection_bg": "#eeeeaa",
-        "match_bg": colors["purple_2"] + "ee",
-        "ui_bg": "#eeffff",
-        "ui_hl": colors["cyan_3"],
-        "hover_bg": colors["white"],
-        "badge_bg": colors["purple_1"],
-        "badge_fg": colors["white"],
-        "button_bg": colors["purple_1"],
-        "button_fg": colors["white"]
-    },
-    "acme_classic": {
-        **base_theme,
-        "theme_name": "Acme Classic",
-        "bg_1": colors["paleyellow_1"],
-        "bg_2": colors["paleyellow_2"],
-        "bg_3": colors["paleyellow_3"],
-        "selection_bg": colors["yellow_2"],
-        "match_bg": colors["purple_2"] + "ee",
         "ui_bg": colors["cyan_4"],
         "ui_hl": colors["cyan_3"],
-        "hover_bg": colors["white"],
-        "badge_bg": colors["purple_1"],
-        "badge_fg": colors["white"],
-        "button_bg": colors["purple_1"],
-        "button_fg": colors["white"]
     },
     "acme_plain": {
         **base_theme,
         "theme_name": "Acme Plain",
-        "bg_1": colors["paleyellow_1"],
-        "bg_2": colors["paleyellow_2"],
-        "bg_3": colors["paleyellow_3"],
-        "selection_bg": colors["yellow_2"],
-        "match_bg": colors["purple_2"] + "ee",
         "ui_bg": colors["paleyellow_1"],
         "ui_hl": colors["paleyellow_3"],
-        "hover_bg": colors["white"],
-        "badge_bg": colors["paleyellow_4"],
-        "badge_fg": colors["black"],
-        "button_bg": colors["purple_1"],
-        "button_fg": colors["white"],
-        "border": "#00000066"
+        "badge_bg": colors["paleyellow_5"],
+        "button_bg": colors["paleyellow_5"],
     },
     "acme_white":{
         **base_theme,
@@ -110,11 +86,6 @@ themes = {
         "match_bg": colors["orange_2"] + "ee",
         "ui_bg": colors["blue_4"],
         "ui_hl": colors["blue_3"],
-        "hover_bg": colors["gray_4"],
-        "badge_bg": colors["purple_1"],
-        "badge_fg": colors["white"],
-        "button_bg": colors["purple_1"],
-        "button_fg": colors["white"]
     }
 }
 
